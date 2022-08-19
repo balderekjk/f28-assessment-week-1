84946de10dedd5cd043cb4fa46cb1147c139194a
Derek Bal <bal.derek.j@gmail.com>
2022-08-19 06:05:31 -1000

    variables done

--- variables.js
+++ variables.js
@@ -1,17 +1,20 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+const myName = 'Derek Bal';
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 13;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +24,12 @@
 */
 
 //CODE HERE
+if (faveNum === 13) {
+  console.log('lucky 13!');
+} else {
+  console.log('not lucky 13');
+}
+//P.S. 13 is actually my favorite number haha, I didn't see this beforehand.
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +37,6 @@
 */
 
 //CODE HERE
+for (let i = 0; i < 5; i++) {
+  console.log(faveNum);
+}
