b66ef3be12ea2d991c2fb1a229c977e066d239ed
Derek Bal <bal.derek.j@gmail.com>
2022-08-19 06:03:01 -1000

    sdlc complete

---
+++ sdlc.js
@@ -0,0 +1,39 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// ANSWER:
+// Planning (Create some structure, know the route)
+// Analaysis of Requirements (Are resources needed available, where/how can they be utilized?)
+// Design (How pages and functions are strung together as well as layouts of single-page)
+// Implementation (Follow the plan)
+// Testing & Integration (Test, test, test! Debug, troubleshoot. Test some more, get a working product)
+// Maintain (Check in routinely, attempt to keep major packages up to date)
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning =
+  'This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.';
+const analysisOfRequirements =
+  'This is the second step, where the requirements of the project must be analyzed. What is necessary for this idea to become real software? The resources for the project must be accessible to move forward.';
+const design =
+  'This is the third step, where all elements of design must be carefully considered. The navigation and flow of the software, and the UI and colors, among other things. Also how in the end the product will be like a cohesive roadmap where everything has practical purpose.';
+const implementation =
+  'This is the third step, where the programmer begins coding. The programmer should already know the exact steps to work on in sequence. This might mean making one functional component at a time and then bringing it all together piece by piece into a larger working application. Or one page at a time. Or with pair programming, everyone assumes a different role or responsibility.';
+const testingAndIntegration =
+  'This is where the developers cover their blindsides by using the software they have coded. Testing must be taken place across all possible platforms of access i.e. Desktop vs. Mobile. All the functions and navigations must be tested to be consistently working.';
+const Maintain =
+  'Post-launch, the product must be kept up to date as much as possible. Checking the versions of major packages up against the current version release and updating all of that, as well as reading the notes for potentially breaking changes that might occur on update. Revising that code and making sure all updates go live.';
