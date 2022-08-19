89cf320a9386d30039064fcdc472093127b1debc
Derek Bal <bal.derek.j@gmail.com>
2022-08-19 06:12:00 -1000

    arrays done

--- arrays.js
+++ arrays.js
@@ -1,14 +1,16 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+const faveColors = ['red', 'green', 'black'];
 
 //CODE HERE
+const colorCopy = faveColors.slice(0, 3);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('maroon');
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,10 +18,10 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 //CODE HERE
-
+const middleNums = numbers.slice(1, numbers.length - 1);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -31,6 +33,14 @@ const numbers = [1, 2, 3, 4, 5]
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
 
 // CODE HERE
+const answers = [];
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push(bigOrSmallArray[i]);
+  } else if (bigOrSmallArray[i] <= 100) {
+    answers.push('small');
+  }
+}
